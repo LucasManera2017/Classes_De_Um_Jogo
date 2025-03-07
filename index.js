a351fc055077cdd04a09285bb5653aa6aca55f24
Lucas Manera <lucas.manera2017@gmail.com>
2025-03-07 09:22:35 -0300

    Adicionado comentarios ao arquivo JS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//Classe Herói Genérica 
 class heroi {
     constructor(nomeHeroi, idadeHeroi, tipoHeroi){
         this.nomeHeroi = nomeHeroi
         this.idadeHeroi = idadeHeroi
         this.tipoHeroi = tipoHeroi
     }
+    //Função atacar com console.log de saída direta 
     atacar(){
         let ataque;
         switch (this.tipoHeroi){
@@ -26,12 +28,14 @@ class heroi {
     }
 }
 
+//Atribuição de valores as propriedades 
 let heroi1 = new heroi("Lucao", 40, "ninja")
 let heroi2 = new heroi("Sergio", 60, "guerreiro")
 let heroi3 = new heroi("Bruce", 33, "mago")
 let heroi4 = new heroi("Tonho", 25, "monge")
 let heroi5 = new heroi("Prado", 100, "bruiser")
 
+//Demonstrando resultados para cada heroi com a função atacar
 heroi1.atacar()
 heroi2.atacar()
 heroi3.atacar()
